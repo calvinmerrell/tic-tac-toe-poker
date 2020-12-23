641fb2dcf104f2b8da08ebd3130f2873fd9affce
Calvin Merrell <73807596+calvinmerrell@users.noreply.github.com>
2020-12-23 05:20:56 -0700

    Create index.js

---
+++ Part2/index.js
@@ -0,0 +1,16 @@
+const idInput = document.getElementById('idInput');
+const colorInput = document.getElementById('colorInput');
+
+
+console.log(idInput);
+console.log(colorInput)
+
+function setCard() {
+  const card = document.getElementById(idInput.value);
+  card.style.color = colorInput.value;
+}
+
+function resetCard(){
+    const card = document.getElementById(idInput.value);
+    card.style.color = null
+}
